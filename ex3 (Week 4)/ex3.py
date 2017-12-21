a47fe2550b82e0f5cecce7b8f544109c72e5af74
sudhap163 <priyadarshini.sudha3@gmail.com>
2017-12-21 23:55:34 +0530

    minor changes

--- ex3 (Week 4)/ex3.py
+++ ex3 (Week 4)/ex3.py
@@ -28,3 +28,23 @@ for i in range(0,10):
     M.append(w)    
 N = np.concatenate((M[0],M[1],M[2],M[3],M[4],M[5],M[6],M[7],M[8],M[9]),axis=0)
 imgplot = plt.imshow(N, cmap='gray')
+
+# costFunction
+
+theta = 0
+
+def sigmoid(z):                     # function to calculate sigmoid value
+    return (1/(1 + np.exp(-z)))
+
+def costFunction( X, Y, theta):
+
+    z = np.dot(X, theta.T)
+    g = sigmoid(z)
+    a = np.multiply(Y, np.log(g))
+    b = np.multiply((1-Y), np.log(1-g))
+    cost = -a-b
+    cost = np.sum(cost)/len(Y)
+    
+    return cost
+
+print(costFunction( X, Y, theta))
