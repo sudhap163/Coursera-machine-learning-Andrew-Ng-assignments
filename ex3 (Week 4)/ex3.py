300755a75880291475bf96603401e8b2cccfa9bc
sudhap163 <priyadarshini.sudha3@gmail.com>
2018-01-12 17:13:42 +0530

    one vs all logistic regression complete

--- ex3 (Week 4)/ex3.py
+++ ex3 (Week 4)/ex3.py
@@ -8,7 +8,7 @@ Created on Wed Dec 20 11:57:29 2017
 import numpy as np
 import scipy.io
 import matplotlib.pyplot as plt
-#import matplotlib.image as mpimg
+from sklearn.linear_model import LogisticRegression
 
 data = scipy.io.loadmat("ex3data1.mat")
 
@@ -24,27 +24,37 @@ m=np.random.choice(range(5000),100,replace=False)
 n = m.tolist()
 M = []
 for i in range(0,10):
-    w = np.concatenate((q[n[i+0]],q[n[i+1]],q[n[i+2]],q[n[i+3]],q[n[i+4]],q[n[i+5]],q[n[i+6]],q[n[i+7]],q[n[i+8]],q[n[i+9]]), axis =1)
+    w = np.concatenate((q[n[(i*10)+0]],q[n[(i*10)+1]],q[n[(i*10)+2]],q[n[(i*10)+3]],q[n[(i*10)+4]],q[n[(i*10)+5]],q[n[(i*10)+6]],q[n[(i*10)+7]],q[n[(i*10)+8]],q[n[(i*10)+9]]), axis =1)
     M.append(w)    
 N = np.concatenate((M[0],M[1],M[2],M[3],M[4],M[5],M[6],M[7],M[8],M[9]),axis=0)
 imgplot = plt.imshow(N, cmap='gray')
 
-# costFunction
+# logistic regression
 
-theta = 0
+X = X.reshape(5000, 400)
+k = np.zeros((10, 5000))
 
-def sigmoid(z):                     # function to calculate sigmoid value
-    return (1/(1 + np.exp(-z)))
+for i in range(0,10):
+    for j in range (0,5000):
+        if (Y[j] == (i+1)):
+            k[i][j] = 1
+        else:
+            k[i][j] = 0
 
-def costFunction( X, Y, theta):
+lr = []
 
-    z = np.dot(X, theta.T)
-    g = sigmoid(z)
-    a = np.multiply(Y, np.log(g))
-    b = np.multiply((1-Y), np.log(1-g))
-    cost = -a-b
-    cost = np.sum(cost)/len(Y)
-    
-    return cost
+for i in range(0,10):
+    lr.append(LogisticRegression())
+    lr[i].fit(X, k[i])
+    if ( i==0 ):
+        probability = lr[i].predict_proba(X)[:,1].reshape(5000,1)
+    else:
+        probability = np.hstack((probability, lr[i].predict_proba(X)[:,1].reshape(5000,1)))
 
-print(costFunction( X, Y, theta))
+newY = (np.argmax(probability, axis = 1) + 1).reshape(5000,1)
+
+error = Y - newY
+
+accuracy = ((5000 - np.nonzero(error)[0].shape[0])/5000)*100
+            
+print ('Accuracy: ', accuracy)
